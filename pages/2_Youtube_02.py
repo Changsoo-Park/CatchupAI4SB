f37ae633413cc9ead91ab29c9da0714ffcef770e
Changsoo-Park <douggy.park@yahoo.com>
2024-06-02 20:53:17 -0700

    Add files via upload

---
+++ pages/2_Youtube_02.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.title('BIGHUG - 소상공인들을 위한 AI')
+st.write('')
+st.write('Youtube Link.')
+st.markdown(""" - [소상공인을 위한 AI 활용법 02](https://youtu.be/lLFdIwrN3Gs?si=LP1Cyu6bpOGaO2aX) """)
+st.write('')
+
+st.video('https://youtu.be/lLFdIwrN3Gs?si=LP1Cyu6bpOGaO2aX')
+
+st.sidebar.header("소상공인을 위한 AI 활용법 02 ☕")
+st.sidebar.write('계속 새로운 비디오가 업로드 됩니다. 기대해 주세요.')
+
+
